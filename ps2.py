98d4bde8fcf76b992b7c1225cce32a778e4614f1
GHANENDRA SINGH <31721853+jtm172022@users.noreply.github.com>
2017-09-12 13:44:27 +0000

    Upadte 3

--- ps2.py
+++ ps2.py
@@ -3,85 +3,51 @@
 ####### write your code here ##########
 
 def tic_tac_toe():
-    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    end = False
-    win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+end = False
+win_commbinations = (sum=15)
 
-    def draw():
-        print(board[0], board[1], board[2])
-        print(board[3], board[4], board[5])
-        print(board[6], board[7], board[8])
-        print()
+# our output look
 
-    def p1():
-        n = choose_number()
-        if board[n] == "X" or board[n] == "O":
-            print("\nYou can't go there. Try again")
-            p1()
-        else:
-            board[n] = "X"
+def draw() :
+print( '\n -----')
+print( '|' + board[0] + '|' + board[1] + '|' + board[2] + '|')
+print( ' -----')
+print( '|' + board[3] + '|' + board[4] + '|' + board[5] + '|')
+print( ' -----')
+print( '|' + board[6] + '|' + board[7] + '|' + board[8] + '|')
+print( ' -----\n')
 
-    def p2():
-        n = choose_number()
-        if board[n] == "X" or board[n] == "O":
-            print("\nYou can't go there. Try again")
-            p2()
-        else:
-            board[n] = "O"
+def p1():
+        	print( "Player 1’s chance")
+		for i in range(0,2)
+			Position = int(raw_input("Enter position"))
+			Number = int(raw_input("Enter Number"))
 
-    def choose_number():
-        while True:
-            while True:
-                a = input()
-                try:
-                    a  = int(a)
-                    a -= 1
-                    if a in range(0, 9):
-                        return a
-                    else:
-                        print("\nThat's not on the board. Try again")
-                        continue
-                except ValueError:
-                   print("\nThat's not a number. Try again")
-                   continue
+		if (Number==1 or Number ==3 or Number == 5 or Number == 7 or Number == 9):
+			Board[Position]=Number
+			printBoard() # Printing Board if valid move
+			if(int(Board[0])+int(Board[1])+int(Board[2]) =15 or (int(Board[3])+int(Board[4])+int(Board[5]) =15 or (int(Board[6])+int(Board[7])+int(Board[8]) =15 or int(Board[0])+int(Board[3])+int(Board[7]) =15 or (int(Board[1])+int(Board[4])+int(Board[7]) =15 or (int(Board[2])+int(Board[5])+int(Board[8]) =15 or int(Board[0])+int(Board[4])+int(Board[8]) =15 or (int(Board[2])+int(Board[4])+int(Board[6]) =15):
+				print("Player 1 wins)
+			
+		else :
+			print("Invalid")
+			continue 
 
-    def check_board():
-        count = 0
-        for a in win_commbinations:
-            if board[a[0]] == board[a[1]] == board[a[2]] == "X":
-                print("Player 1 Wins!\n")
-                print("Congratulations!\n")
-                return True
+def p2():
+        	print( "Player 2’s chance")
+		for i in range(0,2)
+			Position = int(raw_input("Enter position"))
+			Number = int(raw_input("Enter Number"))
 
-            if board[a[0]] == board[a[1]] == board[a[2]] == "O":
-                print("Player 2 Wins!\n")
-                print("Congratulations!\n")
-                return True
-        for a in range(9):
-            if board[a] == "X" or board[a] == "O":
-                count += 1
-            if count == 9:
-                print("The game ends in a Tie\n")
-                return True
+		if (Number==2 or Number ==4 or Number == 6 or Number == 8):
+			Board[Position]=Number
+			printBoard() # Printing Board if valid move
+			if(int(Board[0])+int(Board[1])+int(Board[2]) =15 or (int(Board[3])+int(Board[4])+int(Board[5]) =15 or (int(Board[6])+int(Board[7])+int(Board[8]) =15 or int(Board[0])+int(Board[3])+int(Board[7]) =15 or (int(Board[1])+int(Board[4])+int(Board[7]) =15 or (int(Board[2])+int(Board[5])+int(Board[8]) =15 or int(Board[0])+int(Board[4])+int(Board[8]) =15 or (int(Board[2])+int(Board[4])+int(Board[6]) =15):
+				print("Player 2 wins)
+			
+		else :
+			print("Invalid")
+			continue 
 
-    while not end:
-        draw()
-        end = check_board()
-        if end == True:
-            break
-        print("Player 1 choose where to place a cross")
-        p1()
-        print()
-        draw()
-        end = check_board()
-        if end == True:
-            break
-        print("Player 2 choose where to place a nought")
-        p2()
-        print()
 
-    if input("Play again (y/n)\n") == "y":
-        print()
-        tic_tac_toe()
-
-tic_tac_toe()
